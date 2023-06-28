71f11798e030dd78144fcec1345b52666ad726d2
Irisha <irisha.kostevich@mail.ru>
2023-06-28 19:44:57 +0300

    Add: urls for user information, log out.

--- auth/users/urls.py
+++ auth/users/urls.py
@@ -1,13 +1,13 @@
 # Импортируем модуль path для создания пути к веб-странице.
 from django.urls import path
-# Импортируем класс Registerview для создания пути к веб-странице.
-from .views import Registerview
-# Импортируем класс Loginview для создания пути к веб-странице.
-from .views import Loginview
+# Импортируем класс Registerview, Loginview, UserView для создания пути к веб-странице.
+from .views import Registerview, Loginview, UserView, LogoutView
 
 # Переход к страницам
 urlpatterns = [
     # По пути admin/
     path('register', Registerview.as_view()),
     path('login', Loginview.as_view()),
+    path('user', UserView.as_view()),
+    path('logout', LogoutView.as_view()),
 ]
