82f352d4f5a4d13739f2f94f17d1eb54bdb14d87
Irisha <irisha.kostevich@mail.ru>
2023-12-09 13:17:44 +0300

    Refactoring

--- auth/auth/wsgi.py
+++ auth/auth/wsgi.py
@@ -1,15 +1,11 @@
-"""
-WSGI config for auth project.
+#==========================================================================================#
+# >>>>> ПОДКЛЮЧЕНИЕ БИБЛИОТЕК И МОДУЛЕЙ <<<<< #
+#==========================================================================================#
+from django.core.wsgi import get_wsgi_application
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
 
 import os
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth.settings')
 
--- auth/users/views.py
+++ auth/users/views.py
@@ -6,8 +6,8 @@
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
 from .models import User
+from .serializers import UserSerializer
 
 
 import datetime
